fc156746307cd8427a6707ba1eba2f19cd53c549
rafaelgp3 <rafaelgp3@al.insper.edu.br>
2025-04-09 10:21:01 -0300

    feat: inimigo se movendo

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -5,33 +5,31 @@ using UnityEngine.UI;
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private AudioSource auds;
     private float moveHorizontal;
+    private float moveVertical;
     public float speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        auds = GetComponent<AudioSource>();
         moveHorizontal = 1f;
+        moveVertical = 0f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-    //   float moveHorizontal = Input.GetAxisRaw("Horizontal");
-    //   float moveVertical = Input.GetAxisRaw("Vertical");
-    float moveVertical = 0f;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-      Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-      rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * movement.normalized);  
+        // transform.position += transform.position + speed * Time.fixedDeltaTime * movement.normalized;
+        rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * movement.normalized);  
     }
 
-    private void OnCollisionEnter2D(Collider2D other) {
-        if (other.CompareTag("Objeto")) {
-            moveHorizontal *= -1;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Parede")) {
+            speed *= -1;
         }
     }
 }
