e33d5643b7c8d11c9e6a712b97a210b0777cbb9a
rafaelgp3 <rafaelgp3@al.insper.edu.br>
2025-04-09 18:32:04 -0300

    style: melhor texto de fim

--- Assets/EndTextManager.cs
+++ Assets/EndTextManager.cs
@@ -8,7 +8,26 @@ public class EndTextManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        txtTime.text = $"Tempo final: {TimeManager.tempoPassado}";
-        txtDinheiro.text = $"Dinheiro total: {PlayerMovement.dinheiro}";
+        string tempo = "";
+        int hora = (int)(TimeManager.tempoPassado / 3600);
+        if (hora < 10) {
+            tempo += "0";
+        }
+        tempo += hora.ToString("N0") + ":";
+
+        int min = (int) (TimeManager.tempoPassado / 60);
+        if (min < 10) {
+            tempo += "0";
+        }
+        tempo += min.ToString("N0") + ":";
+
+        float s = TimeManager.tempoPassado % 60;
+        if (s < 10) {
+            tempo += "0";
+        }
+        tempo += s.ToString("F2");
+        txtTime.text = $"Tempo final: {tempo}";
+
+        txtDinheiro.text = $"Dinheiro total: {PlayerMovement.dinheiro}$";
     }
 }
